4f369ce90cd4c0d046fa4164653ca548cabf4f8f
Christian Jaeger <ch@christianjaeger.ch>
2021-08-06 13:49:02 +0100

    tod.c: posix implementation (is this finished?)

--- tod.c
+++ tod.c
@@ -3,6 +3,8 @@
 #include "tod.h"
 #include "util.h"
 
+#ifdef __C64__
+
 void __fastcall__ tod_calibrate(); // tod_init_s.s
 
 static bool tod_init_done = 0;
@@ -42,6 +44,62 @@ unsigned char getTOD(struct TOD *out) {
         retries++;
     }
 }
+
+#else /* not __C64__ */
+
+#include <sys/time.h>
+#include <time.h>
+#include <errno.h>
+
+void tod_init() {
+    // noop
+}
+
+static uint8_t int_to_BCD(uint8_t v) {
+    uint8_t d0 = v % 10;
+    v = v / 10;
+    uint8_t d1 = v % 10;
+    v = v / 10;
+    if (v) DIE("int_to_BCD: remainder after 2 digits: %i", v);
+    return d1*16 + d0;
+}
+
+static void t_int_to_BCD(struct TestStatistics *__test_stats, uint8_t x) {
+    ASSERT_EQ(BCD_to_int(int_to_BCD(x)), x);
+}
+
+/* uint8_t BCD_to_int(uint8_t bcd); */
+
+TEST(int_to_BCD) {
+    ASSERT_EQ(int_to_BCD(28), 0x28);
+    t_int_to_BCD(__test_stats, 4);
+    t_int_to_BCD(__test_stats, 40);
+    t_int_to_BCD(__test_stats, 99);
+}
+
+static uint8_t int_to_BCD_AMPM(uint8_t v) {
+    return int_to_BCD(v); //XXX finish
+}
+
+unsigned char getTOD(struct TOD *out) {
+    struct timeval tv;
+    struct timezone tz;
+    if (gettimeofday(&tv, &tz)) {
+        perror("gettimeofday");
+        DIE_("could not getTOD");
+    }
+    out->sectenths = tv.tv_usec / 100000;
+    time_t tval = tv.tv_sec; /* sgh C lib */
+    struct tm t;
+    if (! gmtime_r(&tval, &t)) DIE_("gmtime_r failed (can this even happen?)");
+    out->sec_BCD = int_to_BCD(t.tm_sec);
+    out->min_BCD = int_to_BCD(t.tm_min);
+    out->hours_BCD_AMPM = int_to_BCD_AMPM(t.tm_hour); //XXX
+    return 0;
+}
+
+#endif /* __C64__ */
+
 
 uint8_t BCD_to_int(uint8_t bcd) {
     return ((bcd & 0xF0) >> 4) * 10 + (bcd & 0x0F);
