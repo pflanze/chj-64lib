eb692b4cda69ee91dbf4f80185794731d038a254
Christian Jaeger <ch@christianjaeger.ch>
2021-08-07 17:35:52 +0100

    benchmark.c: silence g++ warning

--- benchmark.c
+++ benchmark.c
@@ -20,18 +20,18 @@ int32_t time_this(void(*f)(void*), void* ctx, uint16_t numiterations) {
     printf("  end: "); TOD_print(&end);
 #ifdef BENCHMARK_WARN
     printf("\r\n");
-    printf(" start = %"PRIi32" ds\r\n", TOD_to_deciseconds(&start));
-    printf("   end = %"PRIi32" ds\r\n", TOD_to_deciseconds(&end));
+    printf(" start = %" PRIi32 " ds\r\n", TOD_to_deciseconds(&start));
+    printf("   end = %" PRIi32 " ds\r\n", TOD_to_deciseconds(&end));
 #endif
     {
         int32_t d = TOD_diff(&start, &end);
 #ifdef BENCHMARK_DECISECONDS
         d = TOD_diff(&start, &end);
-        printf(" = %"PRIi32"/10 sec", d);
+        printf(" = %" PRIi32 "/10 sec", d);
 #else
         int32_t d_s = d / 10;
         int32_t d_ds = d % 10;
-        printf(" = %"PRIi32".%"PRIi32" sec", d_s, d_ds);
+        printf(" = %" PRIi32 ".%" PRIi32 " sec", d_s, d_ds);
 #endif
         return d;
     }
